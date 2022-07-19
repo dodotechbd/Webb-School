674a0739ca0decc616d1fbd4fb9fb6b13ae2b4d9
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-19 22:53:38 +0600

    added setup again

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,52 +1,88 @@
-import React from 'react';
+import React from "react";
 import logo from '../../Assets/image/logo.png'
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
         <div>
-
-            <footer class="footer p-10 bg-slate-50 text-base-content">
+            <footer class="footer p-10 bg-base-300 text-base-content">
                 <div>
-                    <span class="footer-title">Services</span>
-                    <a class="link link-hover">Book</a>
-                    <a class="link link-hover">Course </a>
-                    <a class="link link-hover">Help</a>
-                    <a class="link link-hover">Our Course</a>
+                    <img className="mx-auto lg:mx-0" width="180" viewBox="0 0 24 24" src={logo} alt="" />
+                    <p className="font-bold text-xl text-center">
+                        Create your free account and start learning
+                    </p>
+                    <button class="btn  text-white btn-sm lg:mx-0 mx-auto">Join Now</button>
                 </div>
-                <div>
-                    <span class="footer-title">Company</span>
-                    <a class="link link-hover">About us</a>
-                    <a class="link link-hover">Contact</a>
-                    <a class="link link-hover">Jobs</a>
-                    <a class="link link-hover">Press kit</a>
+                <div className="grid grid-cols-2 gap-32 mx-auto lg:mx-0">
+                    <div>
+                        <p class="text-xl font-bold text-black mb-2">Services</p>
+                        <a class="link link-hover block">Book Store</a>
+                        <a class="link link-hover block">Courses</a>
+                        <a class="link link-hover block">Admission Help</a>
+                        <a class="link link-hover block">Job Preparation</a>
+                    </div>
+                    <div>
+                        <p class="text-xl font-bold text-black mb-2">Company</p>
+                        <a class="link link-hover block">About us</a>
+                        <a class="link link-hover block">Blogs</a>
+                        <a class="link link-hover block">Career</a>
+                        <a class="link link-hover block">Privacy policy</a>
+                    </div>
                 </div>
-                <div>
-                    <span class="footer-title">Legal</span>
-                    <a href='#'>Rashel</a>
-
-                    <a class="link link-hover">Terms of use</a>
-                    <a class="link link-hover">Privacy policy</a>
-                    <a class="link link-hover">Cookie policy</a>
-                </div>
-            </footer>
-            <footer class="footer px-10 py-4 border-t bg-base-300 text-base-content border-base-300">
-                <div class="items-center grid-flow-col">
-                    <img className='w-16' src={logo} alt='logo' />
-
-                    <p>WEBB SCHOOL. <br /> Providing Educations Purposes since 2022</p>
-                </div>
-                <div class="md:place-self-center md:justify-self-end">
+                <div className="">
+                    <span class="text-xl font-bold text-black">Keep up with us at</span>
+                    <p class="text-md">
+                        <span className="font-bold">Contact:</span> 00000 (8AM - 11PM)
+                    </p>
+                    <p class="text-md">
+                        <span className="font-bold">SMS:</span> WSHelp to 00000 (24X7)
+                    </p>
+                    <p class="text-md">
+                        <span className="font-bold">Email:</span> support@webbschool.com
+                    </p>
                     <div class="grid grid-flow-col gap-4">
-
-                        <a className='text-2xl' href='https://www.linkedin.com/in/rashelmahomud/'><i class="fa-brands fa-linkedin"></i></a>
-                        <a className='text-2xl' href='https://github.com/rashelmahomud'><i class="fa-brands fa-github"></i></a>
-                        <a className='text-2xl' href='https://web.facebook.com/rashelmahomud.raj'><i className="fa-brands fa-facebook"></i></a>
-
+                        <a>
+                            <svg
+                                block xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                class="fill-current"
+                            >
+                                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                            </svg>
+                        </a>
+                        <a>
+                            <svg
+                                block xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                class="fill-current"
+                            >
+                                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                            </svg>
+                        </a>
+                        <a>
+                            <svg
+                                block xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                class="fill-current"
+                            >
+                                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </footer>
-
-
+            <footer class="footer footer-center p-4 bg-base-200 text-base-content">
+                <div>
+                    <p>2022 - {year} Copyright © Webb School. All rights reserved.</p>
+                </div>
+            </footer>
         </div>
     );
 };
