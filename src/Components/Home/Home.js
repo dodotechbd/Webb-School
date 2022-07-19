8965e72ddc08ef6c283bc9dec19d10ec8c3968b6
Foysal Ahamed Badsha <you@example.com>
2022-07-19 09:28:10 +0600

    review section add

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Review from './Review/Review';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home!</h1>
+            <Review></Review>
         </div>
     );
 };
---
+++ src/Components/Home/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div>
+            <h1>This is my review section</h1>
+        </div>
+    );
+};
+
+export default Review;
