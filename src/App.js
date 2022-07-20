c254b7a873029981377701be358f9aea3522ca2e
Foysal Ahamed Badsha <you@example.com>
2022-07-20 11:15:56 +0600

    review image add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Home from "./Components/Home/Home";
+import Review from "./Components/Home/Review/Review";
 
 function App() {
   return (
     <div data-theme="light">
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+      <Review></Review>
     </div>
   );
 }
