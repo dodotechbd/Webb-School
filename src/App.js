9fa4da9beb3019533ae6012013d51e8faff95866
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-07-21 18:25:07 +0600

    Bookstort

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import BookStore from "./Components/BookStore/BookStore/BookStore";
 import Courses from "./Components/Course/Courses";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Shared/Footer";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/courses" element={<Courses></Courses>}></Route>
+        <Route path="/bookstore" element={<BookStore></BookStore>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
