d12e0a0958525340614747b291fadec482657321
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-07-26 11:16:12 +0600

    commit

--- src/Components/BookStore/AcadamicBook/AcadamicBook.js
+++ src/Components/BookStore/AcadamicBook/AcadamicBook.js
@@ -4,12 +4,11 @@ import React from 'react';
 const AcadamicBook = ({acadamicbook}) => {
     const { id, name, img, description, price } = acadamicbook;
     return (
-        <div className=' bg-slate-200 rounded-lg'> 
-            <a href='#' className=" w-100 h-full">
+        <div className=' bg-slate-200 rounded-lg position relative'> 
                 <figure  className="mx-12 pt-10">
                     <img className='w-48   rounded-lg text-center px-' src={img} alt="Books" />
                 </figure>
-                <div class="card-body">
+                <div class="card-body my-7">
                     <h2 class="card-title">
                         <p className='text-2xl font-bold'>{name}</p>
                         <div className="badge badge-primary text-white">Top</div>
@@ -18,7 +17,8 @@ const AcadamicBook = ({acadamicbook}) => {
                     <p className='text-2xl font-bold border-b-2 pb-2'>${price}</p>
             
                 </div>
-            </a>
+    
+                <button class=" btn-block  position absolute bottom-0  bg-slate-400 mt-4  text-center text-xl font-bold rounded-b-lg py-2">Detalis</button>
         </div>
     );
 };
--- src/Components/BookStore/SkillBook/SkillBook.js
+++ src/Components/BookStore/SkillBook/SkillBook.js
@@ -4,12 +4,12 @@ import React from 'react';
 const SkillBook = ({skillbook}) => {
     const { id, name, img, description, price } = skillbook;
     return (
-        <div className=' bg-slate-200 rounded-lg'> 
+        <div className=' bg-slate-200 rounded-lg position relative'  > 
           
                 <figure  className="mx-12 pt-10">
                     <img className='w-48   rounded-lg text-center px-' src={img} alt="Books" />
                 </figure>
-                <div class="card-body">
+                <div class="card-body  mb-7">
                     <h2 class="card-title">
                         <p className='text-2xl font-bold'>{name}</p>
                         <div className="badge badge-primary text-white">Top</div>
@@ -18,10 +18,7 @@ const SkillBook = ({skillbook}) => {
                     <p className='text-2xl font-bold border-b-2 pb-2'>${price}</p>
                    
                 </div>
-       
-
-        
-            <button class=" btn-block  bg-slate-400 mt-4  text-center text-xl font-bold rounded-b-lg py-2">Detalis</button>
+                <button class=" btn-block mt-0 position absolute bottom-0   bg-slate-400 mt-4  text-center text-xl font-bold rounded-b-lg py-2">Detalis</button>
         </div>
     );
 };
