a4b8c6885f0b81e24e1d91a6fd0375ea356f1225
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-26 17:52:50 +0600

    added editing setup

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,22 +2,72 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='lg:mx-20'>
             <h1 className='text-center text-3xl m-20 font-bold'>Know Our Website History</h1>
-            <div class="collapse">
-                <input type="checkbox" />
-                <div class="collapse-title text-xl font-medium text-center">
-                    Read the HTML History <span><button class="btn btn-xs" for="my-modal-5">Show</button>
-                    </span>
-                </div>
+            <div className='grid  sm:grid-cols-2 lg:grid-cols-3 gap-10 my-20'>
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">HTML5</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
 
-                <input type="checkbox" id="my-modal-5" class="modal-toggle" />
-                <div class="modal">
-                    <div class="modal-box w-11/12 max-w-5xl">
-                        <h3 class="font-bold text-lg">Why will you use Meta tag?!</h3>
-                        <p class="py-4">Meta data is a basically used header tag between and using work page description, author of the document, keywords, and viewport settings. tag important its don't show website its work all documents showing system her compare keywords read search engines.</p>
-                        <div class="modal-action">
-                            <label for="my-modal-5" class="btn">Yay!</label>
+                            <label for="my-modal-5" class="btn modal-button">...Show</label>
+
+                            <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+                            <div class="modal">
+                                <div class="modal-box w-11/12 max-w-5xl">
+                                    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                    <div class="modal-action">
+                                        <label for="my-modal-5" class="btn">X</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">CSS3</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+
+                            <label for="my-modal-5" class="btn modal-button">...Show</label>
+
+                            <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+                            <div class="modal">
+                                <div class="modal-box w-11/12 max-w-5xl">
+                                    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                    <div class="modal-action">
+                                        <label for="my-modal-5" class="btn">X</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">JavaScript</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+
+                            <label for="my-modal-5" class="btn modal-button">...Show</label>
+
+                            <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+                            <div class="modal">
+                                <div class="modal-box w-11/12 max-w-5xl">
+                                    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                    <div class="modal-action">
+                                        <label for="my-modal-5" class="btn">X</label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
