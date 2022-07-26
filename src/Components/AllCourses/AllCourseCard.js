ac79b4afeae2b79cd3b78dcd200a3a345fe29ada
MShafiMS <shahedali734@gmail.com>
2022-07-26 13:37:06 -0600

    update courses card

--- src/Components/AllCourses/AllCourseCard.js
+++ src/Components/AllCourses/AllCourseCard.js
@@ -1,63 +1,29 @@
 import React from "react";
 
 const AllCourseCard = ({ allcard }) => {
-  const { name, img, price } = allcard;
+  
   return (
     <a href="#">
       <div
         class="mx-auto card card-compact w-72 bg-base-100 border rounded-md
-        hover:shadow-xl hover:-translate-y-3 border-gray-300  transform transition duration-300"
+       hover:-translate-y-3 border-gray-300  transform transition duration-300"
       >
         <figure>
-          <img src={img} alt="Shoes" className="h-44" />
+          <img src={allcard?.img} alt="Shoes" className="h-44 w-full" />
         </figure>
         <div class="">
           <div className="px-2 pt-1 flex justify-between">
           <p className="text-md font-sans text-gray-500 hover:text-black">
-            Muhammad Shafi
+            {allcard?.instructor}
           </p>
-          <div class="rating rating-sm">
-            <input
-              type="radio"
-              name="rating-3"
-              class="mask mask-heart bg-red-400"
-            />
+          {allcard?.badge ? (<div class="badge badge-secondary">{allcard?.badge}</div>):(<></>)}
           </div>
-          </div>
-          <h2 class="px-2 py-2 text-2xl hover:text-blue-400 font-bold">
-            {name}
+          <h2 class="px-2 py-1 text-xl hover:text-blue-900 font-bold">
+            {allcard?.name.slice(0, 27)}
           </h2>
-          <div class="rating px-2 pb-2">
-            <input
-              type="radio"
-              name="rating-2"
-              class="mask mask-star-2 bg-orange-400"
-            />
-            <input
-              type="radio"
-              name="rating-2"
-              class="mask mask-star-2 bg-orange-400"
-              checked
-            />
-            <input
-              type="radio"
-              name="rating-2"
-              class="mask mask-star-2 bg-orange-400"
-            />
-            <input
-              type="radio"
-              name="rating-2"
-              class="mask mask-star-2 bg-orange-400"
-            />
-            <input
-              type="radio"
-              name="rating-2"
-              class="mask mask-star-2 bg-orange-400"
-            />
-          </div>
           <div className="flex justify-between border-t p-2 text-lg font-bold">
-            <p>৳ 1250</p>
-            <p className="text-success">Buy Now</p>
+            <p>৳ {allcard?.price}</p>
+            <p className="text-blue-900">Buy Now</p>
           </div>
         </div>
       </div>
