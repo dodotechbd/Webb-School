80523db16d0d4f8197ff214b9370c83f85890bea
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-27 19:52:40 +0600

    added blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -46,7 +46,7 @@ const Blogs = () => {
                             <input type="checkbox" id="my-modal-5" class="modal-toggle" />
                             <div class="modal">
                                 <div class="modal-box w-11/12 max-w-5xl">
-                                    <h3 class="font-bold text-lg">Why will you use Meta tag?</h3>
+                                    <h3 class="font-bold text-lg">Why will you use Meta tags?</h3>
                                     <p class="py-4">Meta data is a basically used header tag between and using work page description, author of the document, keywords, and viewport settings.  tag important its don't show website its work all documents showing system her compare keywords read search engines.</p>
 
                                     <h3 class="font-bold text-lg">What are the differences between html4 and html5?  </h3>
@@ -58,25 +58,26 @@ const Blogs = () => {
                             </div>
                         </div>
                     </div>
-                </div>   <div class="card w-96 bg-base-100 shadow-xl image-full">
+                </div>  
+
+                <div class="card w-96 bg-base-100 shadow-xl image-full">
                     <figure><img src={js} alt="Shoes" /></figure>
                     <div class="card-body">
                         <h2 class="card-title">JavaScript</h2>
                         <p>Know Your all JavaScript Q and A</p>
                         <div class="card-actions justify-end">
 
-                            <label for="my-modal-5">...Show</label>
+                            <label for="my-modal">...Shows</label>
 
-                            <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+                            <input type="checkbox" id="my-modal" class="modal-toggle" />
                             <div class="modal">
                                 <div class="modal-box w-11/12 max-w-5xl">
-                                    <h3 class="font-bold text-lg">Why will you use Meta tag?</h3>
+                                    <h3 class="font-bold text-lg">What is JavaScript used for?</h3>
                                     <p class="py-4">Meta data is a basically used header tag between and using work page description, author of the document, keywords, and viewport settings.  tag important its don't show website its work all documents showing system her compare keywords read search engines.</p>
 
-                                    <h3 class="font-bold text-lg">What are the differences between html4 and html5?  </h3>
-                                    <p class="py-4">Html-5 is a Basically html-4 update version and some differences between. Html-5 in remove tag.</p>
+                                    
                                     <div class="modal-action">
-                                        <label for="my-modal-5" class="btn">X</label>
+                                        <label for="my-modal" class="btn">X</label>
                                     </div>
                                 </div>
                             </div>
