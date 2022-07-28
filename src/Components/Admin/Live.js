af22f8e87f45c305bfd3d14c442a82f4b4350b89
MShafiMS <shahedali734@gmail.com>
2022-07-27 23:13:53 -0600

    update admin

--- src/Components/Admin/Live.js
+++ src/Components/Admin/Live.js
@@ -3,8 +3,53 @@ import React from 'react';
 const Live = () => {
     return (
         <div>
-            <h1>Post live class</h1>
+      <div class="hero bg-base-100">
+        <div class="">
+          <div class="text-center my-10">
+            <h1 class="text-3xl font-bold">Post Live Class <i class="fa-solid fa-video text-error"></i></h1>
+          </div>
+          <div class="card flex-shrink-0 w-80 md:w-96 lg:w-96 max-w-sm shadow-2xl bg-red-100">
+            <div class="card-body">
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Category</span>
+                </label>
+                <select class="select select-bordered w-full max-w-xs">
+                  <option>
+                    Language Learning
+                  </option>
+                  <option>Job Requirments</option>
+                  <option>Admission Preparation</option>
+                </select>
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Link</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Link"
+                  class="input input-bordered"
+                />
+              </div>
+              <div class="form-control mt-6">
+                <button class="btn btn-error text-white">Post</button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
     );
 };
 
