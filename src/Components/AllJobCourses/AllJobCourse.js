9fd60b75d9c63c7e5f381a60162694c798be4429
MShafiMS <shahedali734@gmail.com>
2022-07-31 06:56:28 -0600

    create skeleton animation

--- src/Components/AllJobCourses/AllJobCourse.js
+++ src/Components/AllJobCourses/AllJobCourse.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useJobCourses from "../../Hooks/useJobCourse";
 import AllCourseCard from "../AllCourses/AllCourseCard";
+import Loader from "../Shared/Loading/Loader";
 
 const AllJobCourse = () => {
     const [jobcourses] = useJobCourses();
+
+    const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if( loading ){
+    return <Loader></Loader>
+  }
+
   return (
     <div className="px-4"><div className="lg:mx-8 mx-4 pt-10">
       <h1 className="text-3xl pb-5 text-black">Job Recruitment</h1>
