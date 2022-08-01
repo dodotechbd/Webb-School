0cf97657b1b4f60493ca0b9dee53a6a9167c64d8
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-01 22:01:56 +0600

    added daisy

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -4,7 +4,20 @@ const Blog = ({ blog }) => {
     const { name, title, description, address, img } = blog;
     return (
         <div>
-
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        Shoes!
+                        <div class="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <div class="badge badge-outline">Fashion</div>
+                        <div class="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
