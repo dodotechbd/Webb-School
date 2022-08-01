e4e6df6dc01bb971f0dabd571df0bf01b498dfc2
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-01 21:42:12 +0600

    commit

--- src/Components/BookStore/AcadamicBook/AcadamicBook.js
+++ src/Components/BookStore/AcadamicBook/AcadamicBook.js
@@ -12,19 +12,19 @@ const AcadamicBook = ({ acadamicbook }) => {
             <figure className="px-12 pt-4 ">
                 <img className='w-48   rounded-lg text-center px-' src={img} alt="Books" />
             </figure>
+
+
             <div class="card-body px-2  ">
                 <div className="card-body p-0 text-[#0B3456] ">
                     <h1 className="card-title ">
                         <p className=' font-bold'>{name}</p>
                     </h1>
                     <p className=''>{description}</p>
-                    
                     <p className=' font-bold pb-4'>${price}</p>
                 </div>
             </div>
             <button onClick={()=>navigateToAcadamicDetail(_id)} class=" btn-block  position absolute bottom-0  bg-slate-400 mt-4  text-center text-xl font-bold rounded-b-lg py-2  hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456] ">Detalis</button>
         </div>
-
     );
 };
 
