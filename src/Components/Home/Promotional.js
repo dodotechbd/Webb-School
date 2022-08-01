de893a42307243217957393c46ff6a702ba3c488
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-08-01 21:30:33 +0600

    gdgd

--- src/Components/Home/Promotional.js
+++ src/Components/Home/Promotional.js
@@ -3,12 +3,12 @@ import video from "../../Assets/video/promotion.mp4";
 const Promotional = () => {
     return (
         <div>
-              <h1 className='text-3xl font-bold text-center '>Why Choose Us? </h1>
-            <p className="lg:text-xl text-md mb-5 mt-2 text-center text-gray-500">We want to contribute so that learning can be hassle free!!</p>
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row-reverse">
+            <h1 className='text-3xl font-bold text-center '>Why Choose Us? </h1>
+            <p className="lg:text-xl text-md mb-5 mt-2 text-center text-gray-500">We want to contribute so that learning can be hassle freee!!</p>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
                     <video width="700" controls>
-                        <source src={video} type="video/mp4"/>
+                        <source src={video} type="video/mp4" />
                     </video>
                     <div>
                         <h1 class="text-5xl font-bold">We are simply one click away!</h1>
