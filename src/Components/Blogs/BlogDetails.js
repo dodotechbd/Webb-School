91c2e47cb0d3ccdbf51d18e2cd39d3c4be4c774b
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-02 22:29:00 +0600

    added serupt blogs site

--- src/Components/Blogs/BlogDetails.js
+++ src/Components/Blogs/BlogDetails.js
@@ -2,24 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const BlogDetails = () => {
-    const {detailsId} =useParams();
+    const { detailsId } = useParams();
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
         fetch("/blogs.json")
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
 
-    const newBlogs = blogs.filter(blog => blog._id == detailsId )
+    const newBlogs = blogs.filter(blog => blog._id == detailsId)
     console.log(newBlogs);
-    
+
 
     return (
-        <div>
+        <div className='lg:mx-20 my-20'>
 
-            <h1>hello {newBlogs[0]?.name}</h1>
-            
-            
+            <h1 className='text-2xl mt-20 font-bold'>{newBlogs[0]?.title}</h1> <br />
+            <div>
+                <i class="fa-solid fa-user-alien"></i>
+                <h1 className='text-sm '>{newBlogs[0]?.name}</h1>
+            </div>
+
+            <div>
+                <i class="fa-solid fa-house-user"></i>
+                <h1 className='text-sm'>{newBlogs[0]?.address}</h1>
+            </div>
+
+            <div class="">
+
+                <div class="hero-content flex-col lg:flex-row">
+
+                    <img className='w-6/12' src={newBlogs[0]?.img} />
+                    <div>
+                        <h1 class="text-5xl font-bold">Box Office News!</h1>
+                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
