329376927ff3aac28ee4bbcb759a67cfe44bb9eb
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-02 22:30:25 +0600

    added title

--- src/Components/Blogs/BlogDetails.js
+++ src/Components/Blogs/BlogDetails.js
@@ -18,7 +18,15 @@ const BlogDetails = () => {
         <div className='lg:mx-20 my-20'>
 
             <h1 className='text-2xl mt-20 font-bold'>{newBlogs[0]?.title}</h1> <br />
-         
+            <div>
+                <i class="fa-solid fa-user-alien"></i>
+                <h1 className='text-sm '>{newBlogs[0]?.name}</h1>
+            </div>
+
+            <div>
+                <i class="fa-solid fa-house-user"></i>
+                <h1 className='text-sm'>{newBlogs[0]?.address}</h1>
+            </div>
 
             <div class="">
 
@@ -26,7 +34,7 @@ const BlogDetails = () => {
 
                     <img className='w-6/12' src={newBlogs[0]?.img} />
                     <div>
-                        <h1 class="text-5xl font-bold">Box Office News!</h1>
+                        <h1 class="text-5xl font-bold">Box Office</h1>
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
 
                     </div>
