0f8f4a7ac28fddb79f1e2670df766f7242d4cf7b
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-02 23:59:46 +0600

    added blokings site

--- src/Components/Blogs/BlogDetails.js
+++ src/Components/Blogs/BlogDetails.js
@@ -15,35 +15,41 @@ const BlogDetails = () => {
 
 
     return (
-        <div className='lg:mx-20 my-20'>
+        <div className='grid lg:grid-cols-3 grid-cols-1 lg:px-20 my-2 lg:my-20'>
+            <div className='lg:mx-20  col-span-2'>
 
-            <h1 className='text-2xl mt-20 font-bold m-2'>{newBlogs[0]?.title}</h1> <br />
-            <div className='flex'>
-                <i class="fa-solid fa-user-astronaut mx-2 text-secondary"></i>
-                <h1 className='text-sm '>{newBlogs[0]?.name}</h1>
-            </div>
+                <h1 className='text-2xl mt-20 font-bold m-2'>{newBlogs[0]?.title}</h1> <br />
+                <div className='flex'>
+                    <i class="fa-solid fa-user-astronaut mx-2 text-secondary"></i>
+                    <h1 className='text-sm '>{newBlogs[0]?.name}</h1>
+                </div>
 
-            <div className='flex'>
-                <i class="fa-solid fa-house-user mx-2 text-primary"></i>
-                <h1 className='text-sm'>{newBlogs[0]?.address}</h1>
-            </div>
+                <div className='flex'>
+                    <i class="fa-solid fa-house-user mx-2 text-primary"></i>
+                    <h1 className='text-sm'>{newBlogs[0]?.address}</h1>
+                </div>
 
-            <div class="">
+                <div class="">
 
-                <div class="hero-content flex-col lg:flex-row">
+                    <div class=" flex-col lg:flex-row ">
 
-                    <img className='w-6/12' src={newBlogs[0]?.img} />
-                    <div className='m-2'>
-                        <h1 class="text-3xl font-bold">{newBlogs[0]?.title2}</h1>
-                        <p class="py-6">{newBlogs[0]?.add}</p>
-                        <h1 class="text-2xl font-bold">{newBlogs[0]?.title3}</h1>
-                        <p class="py-6">{newBlogs[0]?.add1}</p>
-                        <h1 class="text-2xl font-bold">{newBlogs[0]?.title4}</h1>
-                        <p class="py-6">{newBlogs[0]?.add2}</p>
+                        <img className='my-3' src={newBlogs[0]?.img} />
+                        <div className='m-2'>
+                            <h1 class="text-3xl font-bold">{newBlogs[0]?.title2}</h1>
+                            <p class="py-6">{newBlogs[0]?.add}</p>
+                            <h1 class="text-2xl font-bold">{newBlogs[0]?.title3}</h1>
+                            <p class="py-6">{newBlogs[0]?.add1}</p>
+                            <h1 class="text-2xl font-bold">{newBlogs[0]?.title4}</h1>
+                            <p class="py-6">{newBlogs[0]?.add2}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
+            <div>
+                <h1>Arguably, JavaScript is one of the easiest programming languages to learn, so it serves as a great first language for anyone brand new to coding. Even the most complex lines of JavaScript code can be written one by one, in fragments. It can also be tested in the web browser at the same time.
+</h1>
+            </div>
         </div>
     );
 };
