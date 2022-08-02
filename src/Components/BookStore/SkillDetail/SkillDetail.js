68e6058ecd3772f356def6500c4d635bc23c2174
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-02 21:30:25 +0600

    commit

--- src/Components/BookStore/SkillDetail/SkillDetail.js
+++ src/Components/BookStore/SkillDetail/SkillDetail.js
@@ -18,18 +18,25 @@ const SkillDetail = () => {
         <div className=' grid gap-6 lg:grid-cols-2 py-12  px-6 md:grid-cols-2 sm:grid-cols-2 '>
  
         <div className='grid  justify-items-center  align-middle  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 '>
-        <p>Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant. Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant</p>
-        <figure><img className=' h-full w-96' src={newService[0]?.img} alt="Shoes" /></figure>
+        <p className='font-bold'>Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant. Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant</p>
+        <figure><img className='  w-96' src={newService[0]?.img} alt="Shoes" /></figure>
 
         </div>
-        <div className='  w-96   shadow-xl border-2 p-5 rounded-lg'>
-            <h1 className='text-2xl pragraph-color pb-1 font-bold'>Books Name: <span className='text-2xl text-primary font-bold'>{newService[0]?.name}</span></h1>
-            <p className='pragraph-color text-2xl font-bold'> Writers: {newService[0]?.description}</p>
-            <p className='pragraph-color text-xl font-semibold '> Pages: {newService[0]?.pages}</p>
-            <p className='pragraph-color text-xl font-semibold '><i class="fa-solid fa-file-pdf text-[#efad1e]"></i>Format: {newService[0]?.format}</p>
-            <h3 className='font-semibold  pragraph-color'>{newService[0]?.details}</h3>
-            <h3 className='font-bold text-2xl  pragraph-color'>Price $:  {newService[0]?.price}</h3>
-            <button  class=" btn px-4 bg-indigo-400 mt-4  text-center  font-bold rounded-b-lg p   hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456]">Buy Now</button>
+        <div className='  w-4/5  shadow-xl border-2 p-5 rounded-lg'>
+            <h1 className='text-2xl pragraph-color pb-1 flex justify-center font-bold'>Books Name: <span className='text-2xl text-primary font-bold'>{newService[0]?.name}</span></h1>
+            <div className=' flex justify-around  pt-11'>
+                <div>
+               
+           
+                </div>
+                <div>
+                <p className=' font-bold'><i class="fa-solid fa-file-pen  pr-2  text-[#efad1e]"></i> Writers: {newService[0]?.description}</p>
+                 <p className=' font-semibold '> <i class="fa-solid fa-file   pr-4  text-[#efad1e]"></i> Pages: {newService[0]?.pages}</p>
+                <p className=' font-semibold '><i class="fa-solid fa-file-pdf pr-4 text-[#efad1e]"></i>Format: {newService[0]?.format}</p>
+            <h3 className='font-bold '><i class="fa-solid fa-hand-holding-dollar pr-4 text-[#efad1e]"></i> Price $:  {newService[0]?.price}</h3>  
+                </div>
+            </div>
+            <button  class=" btn-block px-4 bg-indigo-400 mt-4  text-center  font-bold py-2   hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456]">Buy Now</button>
         </div>
     </div>
     </div>
