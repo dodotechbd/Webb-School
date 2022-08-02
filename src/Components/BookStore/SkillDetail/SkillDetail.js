26dd782a6105661561d9d64cd8f84b23bd208620
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-02 19:48:22 +0600

    commit

--- src/Components/BookStore/SkillDetail/SkillDetail.js
+++ src/Components/BookStore/SkillDetail/SkillDetail.js
@@ -17,18 +17,19 @@ const SkillDetail = () => {
             <h1 className='text-4xl text-center pt-6 text-primary font-bold'>Books Detalis</h1>
         <div className=' grid gap-6 lg:grid-cols-2 py-12  px-6 md:grid-cols-2 sm:grid-cols-2 '>
  
-        <div className='grid gap-6 align-middle  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 '>
-        <figure><img className=' h-72 w-96' src={newService[0]?.img2} alt="Shoes" /></figure>
-        <figure><img className='h-72 w-96' src={newService[0]?.img3} alt="Shoes" /></figure>  
+        <div className='grid  justify-items-center  align-middle  sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 '>
+        <p>Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant. Improve grammar, word choice, and sentence structure in your writing. It's free! Grammarly Can Help You Write Polished, Effective Book Reports Every Time. Fix Punctuation Errors. Find and Add Sources Fast. Easily Improve Any Text. AI Writing Assistant</p>
+        <figure><img className=' h-full w-96' src={newService[0]?.img} alt="Shoes" /></figure>
+
         </div>
-        <div className=''>
+        <div className='  w-96   shadow-xl border-2 p-5 rounded-lg'>
             <h1 className='text-2xl pragraph-color pb-1 font-bold'>Books Name: <span className='text-2xl text-primary font-bold'>{newService[0]?.name}</span></h1>
             <p className='pragraph-color text-2xl font-bold'> Writers: {newService[0]?.description}</p>
             <p className='pragraph-color text-xl font-semibold '> Pages: {newService[0]?.pages}</p>
-            <p className='pragraph-color text-xl font-semibold '> Format: {newService[0]?.format}</p>
+            <p className='pragraph-color text-xl font-semibold '><i class="fa-solid fa-file-pdf text-[#efad1e]"></i>Format: {newService[0]?.format}</p>
             <h3 className='font-semibold  pragraph-color'>{newService[0]?.details}</h3>
             <h3 className='font-bold text-2xl  pragraph-color'>Price $:  {newService[0]?.price}</h3>
-
+            <button  class=" btn px-4 bg-indigo-400 mt-4  text-center  font-bold rounded-b-lg p   hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456]">Buy Now</button>
         </div>
     </div>
     </div>
