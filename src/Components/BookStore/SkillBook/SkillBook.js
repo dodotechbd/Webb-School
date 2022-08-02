118d813ed383bf76ab089501acb5ca598cbefeda
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-02 17:29:27 +0600

    commit

--- src/Components/BookStore/SkillBook/SkillBook.js
+++ src/Components/BookStore/SkillBook/SkillBook.js
@@ -14,14 +14,15 @@ const SkillBook = ({skillbook}) => {
                 <figure  className="px-12 pt-4">
                     <img className='w-48   rounded-lg text-center px-' src={img} alt="Books" />
                 </figure>
-                <div class="card-body  px-2 p-0 text-[#0B3456]">
+                <div class="card-body  px-2">
+                <div className="card-body p-0 text-[#0B3456] ">
                     <h1 class="card-title pt-2">
                         <p className='text-xl font-bold'>{name}</p>
                        
                     </h1>
-                    <p className='text-xl'>{description}</p>
-                    <p className='text-2xl font-bold border-b-2 pb-2'>${price}</p>
-                   
+                    <p className=''>{description}</p>
+                    <p className='font-bold pb-4'>${price}</p>
+                    </div>
                 </div>
                 <button  onClick={()=>navigateToAcadamicDetail(_id)} class=" btn-block position absolute bottom-0   bg-slate-400 mt-4  text-center text-xl font-bold rounded-b-lg py-2   hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456]">Detalis</button>
         </div>
