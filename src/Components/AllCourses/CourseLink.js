431b42af7cbc0dbd261ccf85eef03db89b757630
MShafiMS <shahedali734@gmail.com>
2022-08-03 01:35:15 -0600

    polish ui

--- src/Components/AllCourses/CourseLink.js
+++ src/Components/AllCourses/CourseLink.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, NavLink } from "react-router-dom";
 import useAdmission from "../../Hooks/useAdmission";
 import useJobCourses from "../../Hooks/useJobCourse";
 import useLanguage from "../../Hooks/useLanguage";
@@ -14,23 +14,29 @@ const CourseLink = ({ course }) => {
     admission.find((allcard) => allcard.uname === uname) ||
     language.find((allcard) => allcard.uname === uname) ||
     jobcourses.find((allcard) => allcard.uname === uname);
+
+    
   return (
     <>
       <div
         tabindex="0"
-        class="collapse collapse-arrow border-b border-neutral mx-2"
+        class="collapse collapse-arrow border-b border-neutral px-2"
       >
-        <input type="checkbox" class="peer" />
-        <div class="collapse-title hover:bg-accent text-lg font-medium">{course?.fileName}</div>
-        <Link
-               class="collapse-content -mb-2 pt-2 bg-base-100 cursor-pointer"
+        <input type="checkbox"/>
+        <div class="collapse-title text-lg font-medium">{course?.fileName}</div>
+        <ul className="menu collapse-content p-0">
+          <li>
+          <NavLink
+          className='border border-neutral'
               to={`/course/${uname}/${courseData?.list}/${course?.fileName}`}
             >
               <p className="text-md">
           <i class="fa-solid fa-play mr-2"></i>
               {course?.fileName}
           </p>
-            </Link>
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </>
   );
