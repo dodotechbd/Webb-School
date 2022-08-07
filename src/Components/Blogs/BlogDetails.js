d8dbf0a96c64dbe991b1821e4b3f6bf0511fb69c
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-08 01:05:50 +0600

    added backend link

--- src/Components/Blogs/BlogDetails.js
+++ src/Components/Blogs/BlogDetails.js
@@ -5,7 +5,7 @@ const BlogDetails = () => {
     const { detailsId } = useParams();
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
-        fetch("/blogs.json")
+        fetch("http://localhost:5000/blogs")
             .then(res => res.json())
             .then(data => setBlogs(data))
     }, [])
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -5,7 +5,7 @@ const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
-        fetch('blogs.json')
+        fetch('http://localhost:5000/blogs')
             .then(res => res.json())
             .then(data => setBlogs(data))
     }, [])
