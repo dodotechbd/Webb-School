15c409a7f54ceef7c1377337eb4c93b27e9fe979
MShafiMS <shahedali734@gmail.com>
2022-08-07 13:38:53 -0600

    bug fixed

--- src/Components/AllJobCourses/AllJobCourse.js
+++ src/Components/AllJobCourses/AllJobCourse.js
@@ -24,7 +24,7 @@ const AllJobCourse = () => {
       <div className="lg:mx-8 mx-4 pt-10">
         <h1 className="text-3xl pb-5 ">Job Recruitment</h1>
         <div className="grid sm:grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-4 mb-10">
-          {jobcourses.slice(0, 4).map((allcard) => (
+          {jobcourses.map((allcard) => (
             <AllCourseCard key={allcard._id} allcard={allcard}></AllCourseCard>
           ))}
         </div>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
