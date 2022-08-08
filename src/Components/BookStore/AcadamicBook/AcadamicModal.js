efba1ce6995a93fba72362e23e134b4a01daca8d
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-08 11:02:28 +0600

    update bookstore

--- src/Components/BookStore/AcadamicBook/AcadamicModal.js
+++ src/Components/BookStore/AcadamicBook/AcadamicModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AcadamicModal = () => {
+const AcadamicModal = ({book}) => {
     return (
         <div>
 
@@ -10,7 +10,7 @@ const AcadamicModal = () => {
                     <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
                     <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                     <div class="modal-action">
-                        <label for="acadamic-modal" class="btn">Yay!</label>
+                        <label for="acadamic-Modal" class="btn">Yay!</label>
                     </div>
                 </div>
             </div>
