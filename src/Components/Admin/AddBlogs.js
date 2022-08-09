d28dfbff2c414e3ae50572f6ba7e76b7e8b4c610
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-09 22:56:12 +0600

    added input failed

--- src/Components/Admin/AddBlogs.js
+++ src/Components/Admin/AddBlogs.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddBlogs = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+
+        console.log(data);
+    }
+
     return (
         <div>
             <div>
                 <h1 className='text-2xl text-center my-10'>Add Your Blog</h1>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <input
+                        type="text"
+                        placeholder="blogs title"
+                        class="input input-bordered w-full max-w-xs"
+                        {...register("text", {
+                            required: {
+                                value: true,
+                                message: "blogs Is Required"
+                            }
+                        })} /> <br />
+
+                    <textarea
+
+                        type="TextField"
+                        placeholder="blogs title"
+                        class="input input-bordered w-full max-w-xs"
+                        {...register("TextField", {
+                            required: {
+                                value: true,
+                                message: "TextField Is Required"
+                            }
+                        })}
+
+                    /> <br />
+
+                    <input type="submit" className='btn' />
+
+                </form>
             </div>
         </div>
     );
