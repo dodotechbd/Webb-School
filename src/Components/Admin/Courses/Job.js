4e58333e2b46bfd61093e7d5ab61be9424f65b63
MShafiMS <shahedali734@gmail.com>
2022-08-10 07:32:38 -0600

    update tab

---
+++ src/Components/Admin/Courses/Job.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import JobCard from './JobCard';
+
+const Job = () => {
+    const [course, setCourse] = useState();
+  const { data: job, isLoading, refetch } = useQuery(["jobManage"], () =>
+    fetch(`https://rocky-escarpment-87440.herokuapp.com/job`).then((res) =>
+      res.json()
+    )
+  );
+  const deleteItem = (id) => {
+    const remaining = job?.filter((item) => item._id !== id);
+    setCourse(remaining);
+  };
+    return (
+        <div className="text-center">
+        <div className="lg:mx-8 mx-4 pt-10">
+          <h1 className="text-3xl pb-5 ">Job Learning</h1>
+          <div className="grid sm:grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 mb-10">
+            {job?.map((allcard) => (
+              <JobCard deleteItem={deleteItem} key={allcard._id} allcard={allcard} refetch={refetch}></JobCard>
+            ))}
+          </div>
+        </div>
+        <div>
+          <Link className="btn btn-primary normal-case" to={"/admin/post"}>
+            <i className="fa-solid fa-folder-plus mr-2"></i>Add Job Course
+          </Link>
+        </div>
+      </div>
+    );
+};
+
+export default Job;
--- src/Components/Admin/Courses/Manage.js
+++ src/Components/Admin/Courses/Manage.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import Admission from "./Admission";
+import Job from "./Job";
 import Language from "./Language";
 const Courses = () => {
-    
   return (
-    <div>
-      <Language></Language>
+    <div className="text-center mt-4">
+      <ul class="menu menu-horizontal bg-base-300 w-fit rounded-box">
+        <li>
+        <NavLink to={"/admin/courses/language"} className='hover:rounded-none hover:rounded-l-2xl'>Language</NavLink>
+        </li>
+        <li>
+        <NavLink to={"/admin/courses/job"} className='hover:rounded-none'>Job</NavLink>
+        </li>
+        <li>
+        <NavLink to={"/admin/courses/admission"} className='hover:rounded-none hover:rounded-r-2xl'>Admission</NavLink>
+        </li>
+      </ul>
+      <div>
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
