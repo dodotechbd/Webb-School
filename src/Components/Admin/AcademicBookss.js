737e93d9297d0bc660132d05d18f388656d8c81c
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-11 17:50:53 +0600

    bookstore admin

--- src/Components/Admin/AcademicBookss.js
+++ src/Components/Admin/AcademicBookss.js
@@ -1,11 +1,73 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AcademicBookss = () => {
-    return (
-        <div>
-          this is AcadamicBook
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+      console.log("hello", data);
+      const url = `http://localhost:5000/AcadamicBook`;
+      fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-type': 'application/json'
+          },
+
+          body: JSON.stringify(data)
+      })
+          .then(res => res.json())
+          .then(data => {
+              console.log(data);
+          })
+
+  }
+
+  return (
+      <div>
+          <div>
+              <h1 className='text-2xl text-center my-10'>Add Your Books</h1>
+          </div>
+          <div className='text-center'>
+              <form onSubmit={handleSubmit(onSubmit)}>
+
+                  <input
+                      type="text"
+                      placeholder="Books title"
+                      class="input input-bordered w-full max-w-xs"
+                      {...register("title", {
+                          required: {
+                              value: true,
+                              message: "title Is Required"
+                          }
+                      })} /> <br />
+
+                  <textarea
+
+                      type="TextField"
+                      placeholder=" Descriptions"
+                      class="input input-bordered w-full max-w-xs mt-3"
+                      {...register("description", {
+                          required: {
+                              value: true,
+                              message: "TextField Is Required"
+                          }
+                      })}
+
+                  /> <br />
+                  <input
+                      placeholder='Img URL'
+                      className='input input-bordered w-full max-w-xs mt-3'
+                      type="text" {...register("img")}
+
+                  /><br />
+
+
+                  <input type="submit" className='btn w-36 mt-3' />
+
+              </form>
+          </div>
+      </div>
+  );
 };
 
 export default AcademicBookss;
--- src/Components/BookStore/AcadamicBook/AcadamicBook.js
+++ src/Components/BookStore/AcadamicBook/AcadamicBook.js
@@ -9,8 +9,8 @@ const AcadamicBook = ({ acadamicbook }) => {
   return (
     <div
       onClick={() => navigateToAcadamicDetail(_id)}
-      className="btn-ghost bg-base-300 rounded-lg position relative p-0 shadow-lg cursor-pointer mb-4"
-    >
+      className="btn-ghost bg-base-300 rounded-lg position relative p-0 shadow-lg cursor-pointer mb-4">
+
       <figure>
         <img
           className="mx-auto mt-4 w-48 rounded-lg text-center"
@@ -21,7 +21,7 @@ const AcadamicBook = ({ acadamicbook }) => {
       <div className="card-body p-0">
         <div className="card-body p-4">
           <h1>
-            {name.length >= 18 ? (
+            {name?.length >= 18 ? (
               <p className="text-xl font-bold">{name.slice(0, 18)}...</p>
             ) : (
               <p className="text-xl font-bold">{name}</p>
