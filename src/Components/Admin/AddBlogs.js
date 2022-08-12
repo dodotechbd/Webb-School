c5d09b9b7afe3ad625d1b8d28ccbd482abc5db59
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-13 00:00:35 +0600

    palish blogs

--- src/Components/Admin/AddBlogs.js
+++ src/Components/Admin/AddBlogs.js
@@ -23,49 +23,59 @@ const AddBlogs = () => {
     }
 
     return (
+
         <div>
-            <div>
-                <h1 className='text-2xl text-center my-10'>Add Your Blogs</h1>
+
+            <div class="card lg:w-3/5 w-80 bg-base-300 shadow-xl lg:m-10 m-5">
+                <div class="card-body">
+                    <h2 class="card-title text-2xl text-center my-10">Add Your Blogs</h2>
+                    <div>
+
+                        <div>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
+                                <input
+                                    type="text"
+                                    placeholder="blogs title"
+                                    class="input input-bordered w-full"
+                                    {...register("title", {
+                                        required: {
+                                            value: true,
+                                            message: "blogs Is Required"
+                                        }
+                                    })} /> <br />
+
+                                <input
+                                    placeholder='Img URL'
+                                    className='input input-bordered w-full  mt-3'
+                                    type="text" {...register("img")}
+
+                                /><br />
+
+                                <textarea
+
+                                    type="TextField"
+                                    placeholder="blog Descriptions"
+                                    class="input input-bordered w-full mt-3"
+                                    {...register("description", {
+                                        required: {
+                                            value: true,
+                                            message: "TextField Is Required"
+                                        }
+                                    })}
+
+                                /> <br />
+
+                                <input type="submit" className='btn w-full mt-3 ' />
+
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            <div className='text-center'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-
-                    <input
-                        type="text"
-                        placeholder="blogs title"
-                        class="input input-bordered w-full max-w-xs"
-                        {...register("title", {
-                            required: {
-                                value: true,
-                                message: "blogs Is Required"
-                            }
-                        })} /> <br />
-
-                    <textarea
-
-                        type="TextField"
-                        placeholder="blog Descriptions"
-                        class="input input-bordered w-full max-w-xs mt-3"
-                        {...register("description", {
-                            required: {
-                                value: true,
-                                message: "TextField Is Required"
-                            }
-                        })}
-
-                    /> <br />
-                    <input
-                        placeholder='Img URL'
-                        className='input input-bordered w-full max-w-xs mt-3'
-                        type="text" {...register("img")}
-
-                    /><br />
 
 
-                    <input type="submit" className='btn w-36 mt-3' />
-
-                </form>
-            </div>
         </div>
     );
 };
