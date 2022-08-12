d8abca2e0ecda3fa0710e36c28dea4a0d423a27c
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-13 00:01:11 +0600

    added width

--- src/Components/Admin/AddBlogs.js
+++ src/Components/Admin/AddBlogs.js
@@ -26,7 +26,7 @@ const AddBlogs = () => {
 
         <div>
 
-            <div class="card lg:w-3/5 w-80 bg-base-300 shadow-xl lg:m-10 m-5">
+            <div class="card lg:w-3/5 md:w-3/4 w-80 bg-base-300 shadow-xl lg:m-10 m-5">
                 <div class="card-body">
                     <h2 class="card-title text-2xl text-center my-10">Add Your Blogs</h2>
                     <div>
