2a00063831f7e813d5fbabbe107c8dd8a489409a
MDBADSHA7 <mahabubhossainfozlu@gmail.com>
2022-08-14 16:53:11 +0600

    scholarship add

--- src/Scholarship/Scholarship.js
+++ src/Scholarship/Scholarship.js
@@ -9,24 +9,37 @@ const Scholarship = () => {
 
                 <div>
                     <div className='text'>
-                        <p className='normal-text mt-10 ml-20'>
-                            <h1 className='title'>কোর্সে ভালো করলে এককালীন <span>১০০০০০</span> টাকা শিক্ষাবৃওির সুযোগ !!!
-                                <i class="fa-solid fa-sack-dollar dollar"></i>
-                                <i class="fa-solid fa-sack-dollar dollar"></i>
-                                <i class="fa-solid fa-sack-dollar dollar"></i>
-                            </h1>
-                            এই কোর্সের জন্য আমরা একটি প্রাইভেট ফেসবুক গ্রুপ তৈরী করেছি যেখানে কোর্স ইন্সট্রাক্টর মোঃ বাদশাহ সহ আরো 4-5 জন ওয়েব ডেভেলপার সরাসরি আপনাদের বিভিন্ন প্রশ্নের উত্তর দিবেন। আমরা অঙ্গীকার করছি সর্বোচ্চ ২৪ ঘণ্টার মধ্যে আপনাদের প্রশ্নের উত্তর দিতে পারবো। তবে অধিকাংশ ক্ষেত্রে তার চেয়েও কম সময়ে আপনারা উত্তর পেয়ে যাবেন। এছাড়া প্রতি সপ্তাহে আলোচিত মডিউলের উপর লাইভ সেশন থাকবে যেখানে আপনারা সরাসরি প্রশ্ন করে উত্তর পেতে পারবেন।এরপরেও সমস্যার সমাধান না হলে অথবা যেকোনো ধরনের তথ্য জানার প্রয়োজন হলে নিচে দেওয়া নাম্বারে কল অথবা আমাদেরকে ইমেইল করে দিতে পারেন।
+                        <h1 className='title-2 mt-10 ml-20'>কোর্সে ভালো করলে এককালীন <span>১০০০০০</span> টাকা শিক্ষাবৃওির সুযোগ !!!
+                            <i class="fa-solid fa-sack-dollar dollar"></i>
+                            <i class="fa-solid fa-sack-dollar dollar"></i>
+                            <i class="fa-solid fa-sack-dollar dollar"></i>
+                        </h1>
+                        <p className='normal-text mt-5 mr-20 ml-20'>
+                            যারা কোর্সের বিভিন্ন কুইজ, এসাইনমেন্ট ও পরীক্ষায় ভাল রেসাল্ট করবেন এবং সামগ্রিকভাবে কমপক্ষে ৮0% মার্কস পাবেন, তাদের কে নির্বাচিত করে আমরা একটি বিশেষ পরিক্ষার আয়োজন করব। তারপর সেখান থেকে প্রথম <span>৩</span> জনকে ক্রমানুসারে <span>৫০০০০</span> টাকা, <span>৭০০০০</span> টাকা এবং <span>১০০০০০</span> টাকা এককালীন শিক্ষাবৃওি প্রদান করব। জব রেকোমেন্ডেশন লেটার লিখে দিবো। এই কোর্স কাউকে চাকরির নিশ্চয়তা দিতে পারবেনা কারণ কোন কোর্সই সেটা পারেনা। তবে পরিশ্রমী এবং নিবেদিতপ্রাণ লার্নারদের সঠিক গাইডলাইন অবশ্যই নিশ্চিত করবে এবং চাকরির ইন্টারভিউতে যেন তারা আত্মবিশ্বাস এর সাথে ভাল করতে
+                            সহযোগিতা করবে।
                         </p>
-                        <div className='ml-10 icon'>
-                            <a href="tel:+8801735776381"><i className="call fa-solid fa-phone mt-5 ml-10"></i></a>
-                            <a href="https://www.facebook.com/webbSchool"><i className="facebook fa-brands fa-facebook mt-5 ml-10"></i></a>
-                            <a href="Email:mission2023@gmail.com"> <i className="email fa-solid fa-envelope mt-5 ml-10"></i></a>
-
+                        <div className='avatar'>
+                            <div class="avatar placeholder mt-2 ml-20">
+                                <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                                    <span class="text-3xl"><img src="https://scontent.fdac97-1.fna.fbcdn.net/v/t39.30808-6/297814765_1774102069609309_2686621292244915609_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFS1xQW6GlzvVF1ZyKRmvKyC0L1UM6NZBULQvVQzo1kFa6Ozf24q212dVLa1i8IL9tPJGH-Qi2Gw66JluxeCpYm&_nc_ohc=0M-cusPAjP4AX9I3LzV&tn=KQhKgisHJnQegJCF&_nc_ht=scontent.fdac97-1.fna&oh=00_AT9liQzyZyBkGGfTVb5QXar-NhctddpIwWTxJjuz55nMcw&oe=62FD3E27" alt='' /></span>
+                                </div>
+                            </div>
+                            <div class="avatar placeholder mt-2 ml-20">
+                                <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                                    <img src="https://scontent.fdac97-1.fna.fbcdn.net/v/t39.30808-6/243108317_1249895958858795_7713305381357244743_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGcqlQ5pVl5fFeBuFRnVH840HJzXfzc6pvQcnNd_Nzqm68yvJP60EPO8fBL4asGtn77ltFi3eWF6oFdtAAe_3YU&_nc_ohc=S8LjM9OhCTQAX_IlzQw&_nc_ht=scontent.fdac97-1.fna&oh=00_AT_7cwIS4sAtJE8Jtlarzvp7tsHF9WlUZjIBQCBNzqQpWw&oe=62FDF8A4" alt='' />
+                                </div>
+                            </div>
+                            <div class="avatar placeholder mt-2 ml-20">
+                                <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                                    <img src="https://scontent.fdac97-1.fna.fbcdn.net/v/t39.30808-6/211740701_2970174183226716_7543646685170793044_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHQkPjlw3OWFIhYRGh_kaOg3ucBwXFnm7Xe5wHBcWebtfX4CtMDAkzkQ_mdUI0VAyuZGeuUWuBOo7QgPvtsGfA0&_nc_ohc=EXo8kO6z80QAX8q7vNc&_nc_ht=scontent.fdac97-1.fna&oh=00_AT9Kjs2VicWZ-dvzxWA93BFAsvGQZeUwB7zXPfEdrpiOgw&oe=62FDDC41" alt='' />
+                                </div>
+                            </div>
                         </div>
+                        <h1 className='success mt-4'>বিগত বছরের সফল ছাএ।</h1>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
