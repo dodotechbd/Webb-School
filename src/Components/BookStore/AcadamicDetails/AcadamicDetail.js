a2298f07fbd880edf67831be6ecc2623c03b53d9
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-16 22:34:29 +0600

    added coded modal

--- src/Components/BookStore/AcadamicDetails/AcadamicDetail.js
+++ src/Components/BookStore/AcadamicDetails/AcadamicDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
+import { useForm } from 'react-hook-form';
 
 const AcadamicDetail = () => {
 
@@ -14,6 +15,15 @@ const AcadamicDetail = () => {
             .then(res => res.json())
             .then(data => setService(data))
     }, [])
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+
+    };
+
+
 
     const newService = service.filter(s => s._id == acadamicbookId)
     //   console.log(newService[0]);
@@ -107,7 +117,23 @@ const AcadamicDetail = () => {
                         <div class="modal-box relative">
                             <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                             <h3 class="text-lg font-bold">BOOK: {newService[0]?.name}</h3>
-                            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
+
+                                <div class="form-control w-full max-w-xs">
+
+                                    <input type="text" value={user.displayName} placeholder="Type here"
+                                        class="input input-bordered w-full max-w-xs my-2"{...register("book")} />
+
+
+                               
+
+                                </div>
+
+
+                                <button class="btn btn-outline btn-secondary"><input type="submit" /></button>
+
+                            </form>
                         </div>
                     </div>
 
