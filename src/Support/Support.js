1d446f950a26ddd705d46cd4bd0bac28c37b5e41
MDBADSHA7 <mahabubhossainfozlu@gmail.com>
2022-08-17 01:13:57 +0600

    css add

--- src/Support/Support.js
+++ src/Support/Support.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Support.css';
+import './Support.css';
 const Support = () => {
     return (
         <div >
@@ -7,10 +7,9 @@ const Support = () => {
             <div class="card lg:card-side bg-base-100 shadow-xl ml-10 mr-10">
                 <figure><img src="https://www.seqrite.com/skin/frontend/default/seqrite_v1/images/support-img.png" alt="" /></figure>
                 <div class="card-body">
-                    <h1 className='title-1 mt-10 ml-10'>সার্বক্ষণিক সহায়তার জন্য থাকছে
-                        <span> প্রাইভেট সাপোর্ট গ্রুপ - </span> </h1>
+                    <h1 className='title-1 mt-10 ml-8'><span>Private support group</span> where we try to answer within <span>24 hours</span> of our students :</h1>
                     <p className='normal-text mt-10 ml-10'>
-                        এই কোর্সের জন্য আমরা একটি প্রাইভেট ফেসবুক গ্রুপ তৈরী করেছি যেখানে কোর্স ইন্সট্রাক্টর <span>মোঃ বাদশাহ </span> সহ আরো 4-5 জন ওয়েব ডেভেলপার সরাসরি আপনাদের বিভিন্ন প্রশ্নের উত্তর দিবেন। আমরা অঙ্গীকার করছি সর্বোচ্চ ২৪ ঘণ্টার মধ্যে আপনাদের প্রশ্নের উত্তর দিতে পারবো। তবে অধিকাংশ ক্ষেত্রে তার চেয়েও কম সময়ে আপনারা উত্তর পেয়ে যাবেন। এছাড়া প্রতি সপ্তাহে আলোচিত মডিউলের উপর লাইভ সেশন থাকবে যেখানে আপনারা সরাসরি প্রশ্ন করে উত্তর পেতে পারবেন।এরপরেও সমস্যার সমাধান না হলে অথবা যেকোনো ধরনের তথ্য জানার প্রয়োজন হলে নিচে দেওয়া নাম্বারে কল অথবা আমাদেরকে ইমেইল করে দিতে পারেন।
+                        For this course we have created a private Facebook group where course instructor <span>MD BADSHA</span> and 4-5 other web developers will directly answer your various questions. We promise to answer your questions within 24 hours maximum. But in most cases you will get the answer in less time than that. Besides, there will be a live session on the discussed module every week where you can directly ask questions and get answers. Even after that, if the problem is not solved or if you need to know any kind of information, you can call or email us on the number given below.
                     </p>
                     <div className='ml-10 icon'>
                         <a href="tel:+8801735776381"><i className="call fa-solid fa-phone mt-5 ml-10"></i></a>
