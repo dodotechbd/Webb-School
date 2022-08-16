09ae91d23084a5fcfc43df8ac4c9e1ab75a22a64
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-16 22:11:58 +0600

    added modal

--- src/Components/BookStore/AcadamicDetails/AcadamicDetail.js
+++ src/Components/BookStore/AcadamicDetails/AcadamicDetail.js
@@ -95,9 +95,23 @@ const AcadamicDetail = () => {
                         </div>
                     </div>
                     <button className=" btn-block   bg-indigo-400 mt-4  text-center  font-bold py-2   hover:shadow-xl hover:bg-[#0B3456] hover:text-white text-[#0B3456]">Buy Now</button>
+
+                    <label for="my-modal" class="btn modal-button">open modal</label>
+
+                <input type="checkbox" id="my-modal" class="modal-toggle" />
+                <div class="modal">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal" class="btn">Yay!</label>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
+        </div >
     );
 };
 
