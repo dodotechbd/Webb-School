8c407dbbd1db1ff50267f3c1ed0f08214a276f44
MShafiMS <shahedali734@gmail.com>
2022-08-22 12:24:40 -0600

    j

--- src/Components/User/Profile.js
+++ src/Components/User/Profile.js
@@ -36,7 +36,7 @@ const Profile = () => {
       <div
         className="hero"
         style={{  
-          backgroundImage: `url(${user?.data?.coverPhoto ? user?.data?.coverPhoto : "https://im3.ezgif.com/tmp/ezgif-3-4a65346b11.gif"})`,
+          backgroundImage: `url(${user?.data?.coverPhoto ? user?.data?.coverPhoto : "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"})`,
           backgroundPosition: 'center',
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat'
