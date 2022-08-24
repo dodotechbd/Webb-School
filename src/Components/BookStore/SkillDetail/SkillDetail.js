d03525323f09bc54a57ae2d6e2e1decc850b1f62
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-08-24 16:29:55 +0600

    skill book

--- src/Components/BookStore/SkillDetail/SkillDetail.js
+++ src/Components/BookStore/SkillDetail/SkillDetail.js
@@ -25,7 +25,6 @@ const SkillDetail = () => {
     //   console.log(newService[0]);
     return (
         <div>
-        <h1 className='text-4xl text-center pt-6 text-primary font-bold'>Books Details</h1>
         <h1 className='text-2xl px-6  font-bold'>Books Name: <span className='text-2xl text-primary font-bold'>{newService[0]?.name}</span></h1>
         <div className=' grid gap-10 lg:grid-cols-2 py-12  px-6 md:grid-cols-2 sm:grid-cols-2 '>
 
