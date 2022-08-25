76cfca1ee0e1370b2bce63e90d5e0a905953aa72
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-08-26 02:43:36 +0600

    audiobook

--- src/Components/AudioBook/AudioBookDetails.js
+++ src/Components/AudioBook/AudioBookDetails.js
@@ -40,7 +40,103 @@ const AudioBookDetails = () => {
 
                 </div>
                 {/* Mp3 information card */}
-               
+                <div className="lg:w-2/3 md:w-2/3 w-full  bg-[#354795] shadow-xl p-5 card text-white">
+
+                    <div className="p-6 grid items-center gap-6 justify-around lg:grid-cols-2">
+                        <div>
+                            <figure>
+                                <img
+                                    className="w-96 rounded-md"
+                                    src={newDetails[0]?.img}
+                                    alt="Shoes"
+                                />
+                            </figure>
+                        </div>
+                        <div className="grid lg:grid-cols-1 grid-cols-2 gap-3">
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-file-lines  text-green-400"></i>
+                                </p>
+                                <p>
+                                    Addedate <br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.addedate}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-database    text-red-400"></i>
+                                </p>
+                                <p>
+                                    Source <br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.source}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-dice-d6    text-pink-400"></i>
+                                </p>
+                                <p>
+                                    Listener<br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.listener}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-circle-play text-purple-400"></i>
+                                </p>
+                                <p>
+                                    Format <br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.format}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-clock text-teal-400"></i>
+                                </p>
+                                <p>
+                                    Duration <br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.time}
+                                    </span>
+                                </p>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="p-3 bg-[#495CA9] rounded-md mr-4">
+                                    <i className="fa-solid fa-star text-yellow-400"></i>
+                                </p>
+                                <p>
+                                    Average Ratings <br />{" "}
+                                    <span className="uppercase text-[#efad1e]">
+                                        {newDetails[0]?.ratings}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="px-2 pb-2">
+                        <div className="flex text-lg font-bold bg-[#495CA9] p-4 my-4 rounded-xl justify-between">
+                            <div>
+                                <p>Price</p>
+                            </div>
+                            <div>
+                                <p className="text-2xl text-green-400">${newDetails[0]?.price}</p>
+                            </div>
+                        </div>
+                        <Link to={`/bookcheckout/${audiobookId}`}>
+                            <button className="text-lg font-bold p-4 rounded-xl bg-[#efad1e] w-full text-blue-900 hover:bg-secondary hover:text-white">
+                                Buy Now
+                            </button>
+                        </Link>
+                    </div>
+                </div>
             </div>
 
         </div >
