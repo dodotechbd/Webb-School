2db4e92212178bc632f6aeaac3603bc42975a040
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-25 14:37:19 +0600

    added chstas

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -32,44 +32,38 @@ const Chat = () => {
 
             <h1 className='my-5 text-center font-bold text-4xl'> COMMINUCATIONS YOUR FRINDS</h1>
 
-
             <div className='text-center relative'>
 
                 <div class="card w-96 bg-base-100 shadow-xl">
                     <div class="card-body">
-                       
-                    </div>
-                </div>
-
-                <div className="artboard artboard-demo phone-1 ">
-                    <div className="avatar online absolute top-10 ml-40">
-                        <div className="w-8 h-8 rounded-full top-10 ">
-                            <img src={user?.photoURL} />
+                        <div className="avatar online absolute top-10 ml-40">
+                            <div className="w-8 h-8 rounded-full top-10 ">
+                                <img src={user?.photoURL} />
+                            </div>
                         </div>
+                        {chat.map((payload, index) => {
+                            return <p className='text-2xl scroll-auto' key={index}>{payload.message} <span className='font-bold bg-base-300 p-2 rounded'>user: {payload.userName}</span></p>;
+
+                        })}
+
+                        <form onSubmit={sentChat} className='text-center bottom-0 fixed bottom-0 absolute mb-5 mt-2'>
+
+                            <input
+                                type="text"
+                                placeholder="Type here"
+                                name="chat"
+                                value={message}
+                                className="input input-bordered  "
+                                onChange={(e) => {
+                                    setMessage(e.target.value);
+                                }}
+                            />
+                            <button type='submit' className="btn btn-outline btn-accent ">sent</button>
+
+                        </form>
+
+
                     </div>
-
-                    {chat.map((payload, index) => {
-                        return <p className='text-2xl scroll-auto' key={index}>{payload.message} <span className='font-bold bg-base-300 p-2 rounded'>user: {payload.userName}</span></p>;
-
-                    })}
-
-                    <form onSubmit={sentChat} className='text-center bottom-0 fixed bottom-0 absolute mb-5 mt-2'>
-
-                        <input
-                            type="text"
-                            placeholder="Type here"
-                            name="chat"
-                            value={message}
-                            className="input input-bordered  "
-                            onChange={(e) => {
-                                setMessage(e.target.value);
-                            }}
-                        />
-                        <button type='submit' className="btn btn-outline btn-accent ">sent</button>
-
-                    </form>
-
-
                 </div>
 
             </div>
