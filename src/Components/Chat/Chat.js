ec532828fb78152d8b8f0fc331582f52bfa12589
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-27 01:10:53 +0600

    added some code

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,11 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import io from 'socket.io-client';
 import { nanoid } from 'nanoid';
+import { sendEmailVerification } from 'firebase/auth';
+import './style.css';
+
 
 const socket = io.connect("https://rocky-escarpment-87440.herokuapp.com/");
 const userName = nanoid(4);
+
+
+// import sendIcon from './send.svg';
+
+// const generateMessage = () => {
+//     const words = ["The sky", "above", "the port", "was", "the color of television", "tuned", "to", "a dead channel", ".", "All", "this happened", "more or less", ".", "I", "had", "the story", "bit by bit", "from various people", "and", "as generally", "happens", "in such cases", "each time", "it", "was", "a different story", ".", "It", "was", "a pleasure", "to", "burn"];
+//     const text = [];
+//     let x = 7;
+//     while (--x) text.push(words[Math.floor(Math.random() * words.length)]);
+//     return text.join(" ");
+// }
 
 const Chat = () => {
     const [user, loading, error] = useAuthState(auth);
@@ -13,22 +27,55 @@ const Chat = () => {
     const [message, setMessage] = useState("");
     const [chat, setChat] = useState([]);
 
+    const messageEl = useRef(null);
+    const [messages, setMessages] = useState([]);
 
-    const sentChat = (e) => {
-        e.preventDefault();
-        socket.emit("chat", { message, userName });
-        setMessage("");
-    };
+ 
 
-    useEffect(() => {
-        socket.on('chat', (payload) => {
-            setChat([...chat, payload]);
-        });
-    });
+    // useEffect(() => {
+    //     const generateDummyMessage = () => {
+    //         setInterval(() => {
+    //             setMessages(prevMsg => [...prevMsg, generateMessage()]);
+    //         }, 2000);
+    //     }
+    //     generateDummyMessage();
+    // }, []);
+
+
+  
+
 
     return (
         <div>
+            <div className="chat">
+                <div className="head">ChatBot</div>
 
+                <div className="messages" ref={messageEl}>
+                    {chat.map((payload, index) => {
+                        return <p className='text-2xl scroll-auto' key={index}>{payload.message} <span className='font-bold bg-base-300 p-2 rounded'>user: {payload.userName}</span></p>;
+
+                    })}
+                </div>
+
+                <div className="footer">
+                    <form onSubmit={sentChat} className='text-center'>
+
+                        <input
+                            type="text"
+                            placeholder="Type here"
+                            name="chat"
+                            value={message}
+                            className="input input-bordered "
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                        />
+                        <button type='submit' className="btn btn-outline btn-accent ">sent</button>
+
+                    </form>
+                    <img src={sendEmailVerification} />
+                </div>
+            </div>
 
             <h1 className='my-5 text-center font-bold text-4xl'> COMMINUCATIONS YOUR FRINDS</h1>
             {/* <div>
@@ -38,20 +85,21 @@ const Chat = () => {
             <div className='grid justify-items-center relative my-20'>
 
 
-                <div class="card lg:w-2/5 bg-base-300 shadow-xl text-center">
+                <div class="card lg:w-2/5  bg-base-300 shadow-xl text-center">
                     <div class="card-body">
 
-                        {chat.map((payload, index) => {
+                        {/* {chat.map((payload, index) => {
                             return <p className='text-2xl scroll-auto' key={index}>{payload.message} <span className='font-bold bg-base-300 p-2 rounded'>user: {payload.userName}</span></p>;
 
-                        })}
+                        })} */}
 
                     </div>
                 </div>
+
                 <div class="card lg:w-96 bg-base-300 shadow-xl text-center my-3">
                     <div class="card-body">
 
-                        <form onSubmit={sentChat} className='text-center'>
+                        {/* <form onSubmit={sentChat} className='text-center'>
 
                             <input
                                 type="text"
@@ -65,7 +113,7 @@ const Chat = () => {
                             />
                             <button type='submit' className="btn btn-outline btn-accent ">sent</button>
 
-                        </form>
+                        </form> */}
                     </div>
                 </div>
 
