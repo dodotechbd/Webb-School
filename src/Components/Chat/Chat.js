60ed685b0534f818b65a96d5d8b19d372d09182a
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-28 22:26:44 +0600

    added

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -63,7 +63,7 @@ const Chat = () => {
         <div>
             <h1 className='my-10 text-center font-bold text-3xl '>Massage Your Frind</h1>
             <div className="chat m-auto ">
-                <div className="head">Group Chat</div>
+                <div className="head">GROUP CHAT</div>
 
                 <div className="messages" ref={messageEl}>
                     {chat.map((payload, index) => {
